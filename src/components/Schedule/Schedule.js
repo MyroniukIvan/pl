4f8b0759c1f39aabd64c37dee23d9bf133775433
Ivan Myroniuk <95187372+MyroniukIvan@users.noreply.github.com>
2022-11-02 21:26:04 -0400

    justify media and all items

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import autobus from '../../assets/autobus.jpg';
-import autobus1 from '../../assets/autobus1.jpg';
-import flagUkraine from '../../assets/flagUkraine.png';
-import flagPoland from '../../assets/flagPoland.png';
+import map from '../../assets/map.png';
 import "./Schedule.scss";
 
 export const Schedule = () => {
@@ -10,35 +7,7 @@ export const Schedule = () => {
         <div>
             <div className="schedule__block">
                 <div className={"schedule__block-container"}>
-                    <img className={'schedule__block-img'} src={autobus} alt=""/>
-                    <img className={'schedule__block-img'} src={autobus1} alt=""/>
-                </div>
-                <div className="schedule__block-list">
-                    <ul className="schedule__block-list-item">
-                        <li>
-                            <img className={'schedule__block-list-item-flag'} src={flagUkraine} alt="flag of Ukraine"/>
-                            Ukraina
-                        </li>
-                        <li>Lwów</li>
-                        <li>Iwano-Frankowsk</li>
-                        <li>Tarnopol</li>
-                        <li>Czerniowce</li>
-                        <li>Winnica</li>
-                        <li>wyjazd do Polski w każdą niedzielę</li>
-                    </ul>
-                    <ul className="schedule__block-list-item">
-                        <li>
-                            <img className={'schedule__block-list-item-flag'} src={flagPoland} alt="flag of Poland"/>
-                            Polska
-                        </li>
-                        <li>Warszawa</li>
-                        <li>Łódź</li>
-                        <li>Częstochowa</li>
-                        <li>Katowice</li>
-                        <li>Kraków</li>
-                        <li>Rzeszów</li>
-                        <li>wyjazd na Ukrainę w piątki</li>
-                    </ul>
+                    <img className={'map'} src={map} alt=""/>
                 </div>
             </div>
         </div>
