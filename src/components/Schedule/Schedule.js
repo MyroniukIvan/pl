d60a3975d02916061ddbde2f39a60b636ca82ab0
Ivan Myroniuk <95187372+MyroniukIvan@users.noreply.github.com>
2022-11-03 11:07:31 -0400

    justify media and all items

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -3,14 +3,17 @@ import map from '../../assets/map.png';
 import "./Schedule.scss";
 
 export const Schedule = () => {
+
+
     return (
         <div>
             <div className="schedule__block">
                 <div className={"schedule__block-container"}>
-                    <img className={'map'} src={map} alt=""/>
+                    <img id={'map'} className={'map'} src={map} alt=""/>
                 </div>
             </div>
         </div>
+
     );
 };
 
