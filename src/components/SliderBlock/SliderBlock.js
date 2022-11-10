51e8382f2236618ea30e0678e4b50aa9a94dfbd5
Ivan Myroniuk <95187372+MyroniukIvan@users.noreply.github.com>
2022-11-09 20:02:54 -0500

    complete project

--- src/components/SliderBlock/SliderBlock.js
+++ src/components/SliderBlock/SliderBlock.js
@@ -10,17 +10,18 @@ export const SliderBlock = () => {
                 <div className="aboutUs__items-title">
                     <div className="aboutUs__items-header">
                         Nasze produkty
-                        <div className="finger"><i className="fa-regular fa-hand-point-right"></i></div>
+                        <div className="finger"><i className="fa-solid fa-arrow-right"></i></div>
 
                     </div>
                 </div>
                 <div className="aboutUs__items-list">
                     <ul>
-                        <li>Kreda techniczna
+                        <li><i className="fa-solid fa-check"></i>Kreda techniczna
                             Wykorzystywana w wielu Galeziach przemyslu
                         </li>
-                        <li>Wapno ekologiczne</li>
-                        <li>Nawozy mineralne</li>
+                        <li><i className="fa-solid fa-check"></i>Wapno ekologiczne</li>
+                        <li><i className="fa-solid fa-check"></i>Nawozy mineralne</li>
+                        <li><i className="fa-solid fa-check"></i>Dysponujemy wlasnym transportem</li>
                     </ul>
                 </div>
             </div>
